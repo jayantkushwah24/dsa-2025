56faa9a4e14b4dddf704f6c24979e6123fbb0057
Jayant Kushwah <jayantkushwah8@gmail.com>
2025-09-17 17:59:32 +0530

    function

---
+++ typeCasting.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a = 5;
+  float b = 10.5;
+  float sum = a + b;
+  // cout << sum << endl;
+
+  char c = 'a';
+  char d = c + 1;
+  // cout << d;
+
+  float pi = 3.1416;
+  float intPi = (int)pi;
+  cout << intPi;
+
+  // int/int = int
+  // float/int = float
+  // int/float = float
+}
